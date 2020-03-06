435be0a29aa90da5cece0948e2c029c8a2a1ba59
fitkovich1 <fitkovich1@gmai.com>
2020-03-06 14:18:33 +0300

    Updates

---
+++ precache-manifest.1f83e4b6231885242e0919fa47314c7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40549fc63e6566a671097207dff3a4a8",
+    "url": "/descryptor-test/index.html"
+  },
+  {
+    "revision": "13ce49a2e4a747ccb829",
+    "url": "/descryptor-test/static/css/main.0c78421f.chunk.css"
+  },
+  {
+    "revision": "658ea620c95548418b90",
+    "url": "/descryptor-test/static/js/2.df75efe1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/descryptor-test/static/js/2.df75efe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ce49a2e4a747ccb829",
+    "url": "/descryptor-test/static/js/main.c2eda4ac.chunk.js"
+  },
+  {
+    "revision": "550ca859cf574f9da7b4",
+    "url": "/descryptor-test/static/js/runtime-main.1f533b03.js"
+  },
+  {
+    "revision": "af2a27f718981a8a00d655d859843b70",
+    "url": "/descryptor-test/static/media/enigma.af2a27f7.png"
+  }
+]);
